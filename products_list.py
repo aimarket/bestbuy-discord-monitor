9c07647323c749e73b02dd350d81b866318deb48
aimarket <32007930+aimarket@users.noreply.github.com>
2020-05-19 22:54:10 -0700

    Add files via upload

---
+++ products_list.py
@@ -0,0 +1,9 @@
+"""
+=====================================================
+Insert Best Buy website in list, seperated by a comma
+===================================================== 
+"""
+
+products_list =[#e.g."https://www.bestbuy.com/site/combo/nintendo-switch/",
+
+                ]
