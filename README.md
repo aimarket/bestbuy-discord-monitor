ea9b3c1e0e796f1fb6d5fc832212d54311ffe250
andyroo <andjones100@gmail.com>
2020-05-20 00:52:23 -0700

    Moved main program into function main().
    - Refactor some variable names.
    - Move proxy list initiation into function
    -

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Best_Buy_Logo.svg/800px-Best_Buy_Logo.svg.png" alt="drawing" width="200" height="140"/>
 
-## *follow instructions in the main file
-Detects if a product is back in stock and will notify on Discord with webhook
+## Follow instructions in the main file
+Detects if a product is back in stock and will notify on Discord with webhook.
 
-This uses the selenium webdriver for javascript websites(BESTBUY) to load in a headless browser.
+This uses the selenium webdriver for JavaScript websites (BESTBUY) to load in a headless browser.
 
-I tried my best to add comments but if something needs better explination please let me know.
+I tried my best to add comments but if something needs better explanation please let me know.
 
 ## Dependencies:
 >selenium
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ TIME: Line 70 Please change this to reflect the number of proxies
 
 Products: Products_list.py enter product(s) url as a string in the dictionary 
 
-PROXIES: Enter https proxies in Proxy-List.txt
+PROXIES: Enter https proxies in proxy_list.txt
     For example:
         HOST:PORT
         209.97.138.116:8080
@@ -40,45 +40,24 @@ Future Updates hopefully:
 =================================================================================================
 """
 
-#initiate proxy list and format for selenium
-proxy_dict = {}
-with open('Proxy-List.txt') as li:
-    proxy = li.readline()
-    cnt = 0
+def initiate_proxy_list(proxy_file):
+    """
+    Initiate proxy list and format for selenium
+
+    proxy_file: file containing proxies separated by each line
     
-    while proxy:
-        proxy_dict[cnt] = {"http":"{}".format(proxy.strip())}
-        proxy = li.readline()
-        cnt += 1
-li.close()
+    Returns: proxy_list, a list of proxies
+    """
+    proxy_list = []
+    with open(proxy_file) as f:
+        for proxy in f:
+            proxy_list.append({"http":"{}".format(proxy.strip())})
 
-#booleans
-online = True
-timeoutonweb = False
-
-# number of items
-header_length = header_dict.__len__()
-proxy_length = proxy_dict.__len__()
-product_length = products_list.__len__()
-
-#counters dont touch!!!
-headercounter = 0
-proxycounter = 0
-productcounter = 0
-
-#be reasonable, unless you have a long list of proxies, stick with longer delay
-delaylimit = 15
-
-#Proxy organization
-timeoutdict = {}
-#discord post
-discord_queue = []
-webhook = 'YOUR WEBHOOK ON A STRING'
+    return proxy_list
 
 
 
-
-#send to discord
+# Send to discord
 def send_notification(code, website):
     data = {}
     embed = {}
@@ -102,7 +81,7 @@ def send_notification(code, website):
     requests.post(webhook,data=json.dumps(data), headers={"Content-Type": "application/json"})
     #print("posting on discord",website)
 
-#check status of add-to-cart-button
+# Check status of add-to-cart-button
 def check_status(html):
     raw_page = soup.BeautifulSoup(html,'html.parser')
     try:
@@ -117,7 +96,7 @@ def check_status(html):
         return True
 
 def proxyorganize(dic):
-    #makes key values in dictonary in order
+    # Makes key values in dictonary in order
     p = []
     new_dic={}
     for x in list(range(dic.__len__())):
@@ -125,7 +104,7 @@ def proxyorganize(dic):
     new_dic=dict(zip(p,list(dic.values()))) 
     return(new_dic)
 
-def resetcounter(counter, length):
+def reset_counter(counter, length):
     #if the counter reaches the end of the list reset
     if(counter >= length-1):
         counter = 0
@@ -137,117 +116,152 @@ def listmaker(length):
     #random list of numbers with size of length from 0 to length
     return random.sample(range(length),length)
 
-'''
-Main program in a while loop of spagetti code
-TODO optimize for easy redability
-'''
-while(online):
-    # if proxies run out
-    if (len(timeoutdict)>proxy_length):
-        break
+# TODO: Optimize block for readability
+def main():
 
-    #random number generators
-    if(headercounter == 0):
-        headerlist = listmaker(header_length)
-    if(proxycounter == 0):
-        proxylist = listmaker(proxy_length)
-    if(productcounter == 0):
-        productlist = listmaker(product_length)
-    #print(proxylist)
+    # TODO: reformat using list for proxies
+    # Initiate proxy list and format for selenium
+    proxy_dict = initiate_proxy_list('proxy_list.txt')
+    print(proxy_dict)
 
-    #selenium setup
-    options = Options()
-    options.page_load_strategy = 'normal'
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    #options.add_argument("--no-sandbox") # linux only
+    online = True
+    timeoutonweb = False
 
-    #headless mode needs both arguments below
-    options.add_argument("--headless")
-    options.add_argument("window-size=1920x1080")
+    HEADER_LENGTH = header_dict.__len__()
+    PROXY_LENGTH = proxy_dict.__len__()
+    PRODUCT_LENGTH = products_list.__len__()
 
-    # send request to GET website
-    try:
+    # Counters dont touch!!!
+    HEADER_CNT = 0
+    PROXY_CNT = 0
+    PRODUCT_CNT = 0
+
+    #be reasonable, unless you have a long list of proxies, stick with longer delay
+    delaylimit = 15
+
+    #Proxy organization
+    timeoutdict = {}
+
+    #discord post
+    discord_queue = []
+    webhook = 'YOUR WEBHOOK ON A STRING'
+
+    while(online):
+        # if proxies run out
+        if (len(timeoutdict)>PROXY_LENGTH):
+            break
+
+        #random number generators
+        if(HEADER_CNT == 0):
+            headerlist = listmaker(HEADER_LENGTH)
+        if(PROXY_CNT == 0):
+            proxylist = listmaker(PROXY_LENGTH)
+        if(PRODUCT_CNT == 0):
+            productlist = listmaker(PRODUCT_LENGTH)
+        #print(proxylist)
+
+        #selenium setup
+        options = Options()
+        options.page_load_strategy = 'normal'
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        #options.add_argument("--no-sandbox") # linux only
+
+        #headless mode needs both arguments below
+        options.add_argument("--headless")
+        options.add_argument("window-size=1920x1080")
+
+        # send request to GET website
+        try:
+            
+            PROXY = proxy_dict[proxylist[PROXY_CNT]]['http']
+
+            webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
+                "httpProxy":PROXY,
+                "ftpProxy":PROXY,
+                "sslProxy":PROXY,
+                #"noProxy":None,
+                "proxyType":"MANUAL",
+                #"class":"org.openqa.selenium.Proxy",
+                #"autodetect":True
+            }
+            profile = webdriver.FirefoxProfile()
+            profile.set_preference("general.useragent.override", header_dict[headerlist[HEADER_CNT]]['User-Agent'])
+            driver = webdriver.Firefox(options=options,firefox_profile=profile)
+            #website request timeout
+            driver.set_page_load_timeout(20)
+            #serches url in the product_list.py file
+            driver.get(products_list[productlist[PRODUCT_CNT]])
+            #Grab html content
+            html = driver.page_source
+        except TimeoutException as e:
+            timeoutonweb = True
         
-        PROXY = proxy_dict[proxylist[proxycounter]]['http']
 
-        webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
-            "httpProxy":PROXY,
-            "ftpProxy":PROXY,
-            "sslProxy":PROXY,
-            #"noProxy":None,
-            "proxyType":"MANUAL",
-            #"class":"org.openqa.selenium.Proxy",
-            #"autodetect":True
-        }
-        profile = webdriver.FirefoxProfile()
-        profile.set_preference("general.useragent.override", header_dict[headerlist[headercounter]]['User-Agent'])
-        driver = webdriver.Firefox(options=options,firefox_profile=profile)
-        #website request timeout
-        driver.set_page_load_timeout(20)
-        #serches url in the product_list.py file
-        driver.get(products_list[productlist[productcounter]])
-        #Grab html content
-        html = driver.page_source
-    except TimeoutException as e:
-        timeoutonweb = True
-    
+        # debugging
+        date = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        if(timeoutonweb==False):
+            status = check_status(html)
+            #print('status ', status)
+            if(status):
+                if(products_list[productlist[PRODUCT_CNT]] not in discord_queue):
+                    #print("website not in queue")
+                    discord_queue.append(products_list[productlist[PRODUCT_CNT]])
+                    send_notification(1,products_list[productlist[PRODUCT_CNT]])
+            else:
+                # product sold out again
+                for itm in discord_queue:
+                    if(itm == products_list[productlist[PRODUCT_CNT]]):
+                        discord_queue.pop(itm)
+                        send_notification(2,products_list[productlist[PRODUCT_CNT]])
 
-    # debugging
-    date = strftime("%Y-%m-%d %H:%M:%S", localtime())
-    if(timeoutonweb==False):
-        status = check_status(html)
-        #print('status ', status)
-        if(status):
-            if(products_list[productlist[productcounter]] not in discord_queue):
-                #print("website not in queue")
-                discord_queue.append(products_list[productlist[productcounter]])
-                send_notification(1,products_list[productlist[productcounter]])
+            # TODO: make a document to log all this information for debugging
+            print(products_list[productlist[PRODUCT_CNT]][12:],
+                  "online at ", date, " code: ", 200, " using header #", headerlist[HEADER_CNT], "proxy ",
+                  proxy_dict[proxylist[PROXY_CNT]]['http']," #", proxylist[PROXY_CNT])
         else:
-            # product sold out again
-            for itm in discord_queue:
-                if(itm == products_list[productlist[productcounter]]):
-                    discord_queue.pop(itm)
-                    send_notification(2,products_list[productlist[productcounter]])
-        #TODO make a document to log all this information for debugging
-        print(products_list[productlist[productcounter]][12:], "online at ", date, " code: ", 200, " using header #", headerlist[headercounter], "proxy ",proxy_dict[proxylist[proxycounter]]['http']," #", proxylist[proxycounter])
-    else:
-        print(products_list[productlist[productcounter]][12:], "offline at", date, " code", 400, " using header #",headerlist[headercounter], "proxy ",proxy_dict[proxylist[proxycounter]]['http']," #", proxylist[proxycounter])
+            print(products_list[productlist[PRODUCT_CNT]][12:], "offline at", date,
+                  " code", 400, " using header #",headerlist[HEADER_CNT],
+                  "proxy ",proxy_dict[proxylist[PROXY_CNT]]['http']," #", proxylist[PROXY_CNT])
+            
+            # put proxie in timeout dictoinary
+            time_key = time.time()
+            timeoutdict[time_key]=proxy_dict[proxylist[PROXY_CNT]]
+            # take proxie out of active proxies
+            proxy_dict.pop(proxylist[PROXY_CNT])
+            # reorder dictionary
+            proxy_dict = proxyorganize(proxy_dict)
+            # increase time per request
+            delaylimit += 2
+            timeoutonweb = False
+            # used to make new list of random numbers
+            PROXY_CNT = proxy_dict.__len__()
         
-        # put proxie in timeout dictoinary
-        time_key = time.time()
-        timeoutdict[time_key]=proxy_dict[proxylist[proxycounter]]
-        # take proxie out of active proxies
-        proxy_dict.pop(proxylist[proxycounter])
-        # reorder dictionary
-        proxy_dict = proxyorganize(proxy_dict)
-        # increase time per request
-        delaylimit += 2
-        timeoutonweb = False
-        # used to make new list of random numbers
-        proxycounter = proxy_dict.__len__()
-    
-    #add proxie back into active
-    for x in timeoutdict.keys():
-        if(x+400 < time.time()):
-            proxy_dict[proxy_dict.__len__()] = timeoutdict[x]
-            proxycounter = proxy_dict.__len__()
-            timeoutdict.pop(x)
+        #add proxie back into active
+        for x in timeoutdict.keys():
+            if(x+400 < time.time()):
+                proxy_dict[proxy_dict.__len__()] = timeoutdict[x]
+                PROXY_CNT = proxy_dict.__len__()
+                timeoutdict.pop(x)
 
 
 
-    #nap time
-    time.sleep(random.uniform(10.0,delaylimit))
-    #update proxy size
-    proxy_length = proxy_dict.__len__()
+        #nap time
+        time.sleep(random.uniform(10.0,delaylimit))
+        #update proxy size
+        PROXY_LENGTH = proxy_dict.__len__()
 
-    #reset counter after max items are reached
-    headercounter = resetcounter(headercounter,header_length)
-    proxycounter = resetcounter(proxycounter, proxy_length)
-    productcounter = resetcounter(productcounter, product_length)
+        #reset counter after max items are reached
+        HEADER_CNT = reset_counter(HEADER_CNT, HEADER_LENGTH)
+        PROXY_CNT = reset_counter(PROXY_CNT, PROXY_LENGTH)
+        PRODUCT_CNT = reset_counter(PRODUCT_CNT, PRODUCT_LENGTH)
 
-    #kill driver window
-    driver.quit()
+        #kill driver window
+        driver.quit()
 
-send_notification(0,"Notify Creator I used up my proxies :(")
-exit()
+    send_notification(0,"Notify Creator I used up my proxies :(")
+    exit()
+
+
+if __name__ == '__main__':
+    main()
